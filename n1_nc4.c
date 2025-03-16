8473e37571bb38e8ad22f92a7e7fbcce88dfa4b6
Matheus M. <matheusaew@icloud.com>
2025-03-16 13:50:48 -0300

    Update n1_nc4.c

--- n1_nc4.c
+++ n1_nc4.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+int main(){
+    int idade = 30;
+    int quantidade = 1;
+    float altura = 1.74;
+    double peso = 92.2;
+    char opcao = 'S'
+    char letra = 'A';
+    char nome[20] = "Matheus";
+  
+    printf("A idade do %s é: %d\n" , nome, idade);
+    printf("A altura é: %.2f\n", altura);
+    printf("A opção é: %c\n", opcao);
+
+    /*
+    printf("%formato1 %formato2 %formato3, variavel1,")
+    
+    
+    
+    
+    */
